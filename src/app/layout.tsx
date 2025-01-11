a181a6782a5bc31740f5a614e39c2bc2210b8d69
polgarj <joco.polgar@gmail.com>
2025-01-11 19:30:06 +0100

    Add suspense boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,33 +3,44 @@ import "./globals.css";
 import Navigation from "@/components/Navigation/Navigation";
 import UserMenu from "@/components/UserMenu/UserMenu";
 import { Poppins } from "next/font/google";
+import { Suspense } from "react";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
+	subsets: ["latin"],
+	weight: ["400", "500", "600"],
 });
 
 export const metadata: Metadata = {
-  title: "AI - Hammer Agency",
-  description: "Placeholder for description",
+	title: "AI - Hammer Agency",
+	description: "Placeholder for description",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`min-h-screen bg-[url('/background.svg')]  bg-center bg-cover bg-no-repeat ${poppins.className}`}>
-      <header>
-        <Navigation />
-        <UserMenu />
-      </header>
-      <div className="pt-[89px]">
-        {children}
-      </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`min-h-screen bg-[url('/background.svg')]  bg-center bg-cover bg-no-repeat ${poppins.className}`}
+			>
+				<header>
+					<Navigation />
+					<UserMenu />
+				</header>
+				<div className="pt-[89px]">
+					<Suspense
+						fallback={
+							<main className="main-container">
+								<h1 className="h1">Loading...</h1>
+							</main>
+						}
+					>
+						{children}
+					</Suspense>
+				</div>
+			</body>
+		</html>
+	);
 }
